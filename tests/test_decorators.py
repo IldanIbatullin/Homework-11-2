506b6285f535758a2733a5071e581d682d099284
Ildan Ibatullin <rustam162606@yandex.ru>
2024-12-21 18:14:35 +0300

    Написание тестов для декоратора log

---
+++ tests/test_decorators.py
@@ -0,0 +1,31 @@
+import pytest
+import os
+from decorators.decorators import log
+
+@log("logs/test_log.txt")
+def test_function(x, y):
+    return x + y
+
+def test_log_success(capfd):
+    result = test_function(3, 4)
+    assert result == 7
+    captured = capfd.readouterr()
+    assert "test_function ok" in captured.out
+
+def test_log_error(capfd):
+    with pytest.raises(TypeError):
+        test_function(3, 'a')
+    captured = capfd.readouterr()
+    assert "test_function error:" in captured.out
+
+def test_log_file_creation():
+    log_file_path = "logs/test_log.txt"
+    if os.path.exists(log_file_path):
+        os.remove(log_file_path)
+
+    @log(log_file_path)
+    def dummy_function():
+        return "test"
+
+    dummy_function()
+    assert os.path.exists(log_file_path)
